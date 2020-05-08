6c64e2cd28101ca1c7daafb3fda9ef194147dc8d
przemskydev <przemekgithub@o2.pl>
2020-05-08 16:23:36 +0200

    storybook knobs

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import Button from './Button';
 
 export default {
   component: Button,
-  title: 'Button',
+  title: 'Storybook Knobs',
+  decorators: [withKnobs],
 };
 
-export const text = () => <Button secondary>Hello Button</Button>;
+export const text = () => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(49, 100%, 58%)',
+    Secondary: 'hsl(169, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const groupId = 'GROUP-ID1';
+
+  const value = select(label, options, defaultValue, groupId);
+
+  return <Button color={value}>Hello Button</Button>;
+};
 
 export const emoji = () => (
-  <Button>
+  <Button secondary>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
