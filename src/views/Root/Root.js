3d54eb3544af0774a25a462089d75b845f6f2f9b
przemskydev <przemekgithub@o2.pl>
2020-05-08 16:54:37 +0200

    theme provider

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Button from 'components/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello World!</h1>
-    <Button>Close</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <h1>Hello World!</h1>
+      <Button>Close</Button>
+      <Button secondary>Remove</Button>
+    </ThemeProvider>
   </div>
 );
 
