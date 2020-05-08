8686419484b690e0055985959ea4053862a9c851
przemskydev <przemekgithub@o2.pl>
2020-05-08 13:27:41 +0200

    create global style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,11 +2,11 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   background-color: #ffd82b;
-  width: 220px;
+  width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-family: 'Montserrat';
+  font-family: 'Montserrat', sans-serif;
   font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello World!</h1>
     <Button>Close</Button>
     <Button secondary>Remove</Button>
